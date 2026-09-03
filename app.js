23086a86b81b5e7a2dce77e281352267991cdbf6
Javier Morales <javier.morales.arenas94@gmail.com>
2026-09-03 14:08:46 -0600

    Se agregaron los demas archivos para el ejercicio

---
+++ app.js
@@ -0,0 +1,19 @@
+/**
+ * Todo lo que seleccionamos al principio
+ * debe ser seleccionado mediante el documento 
+ * 
+ * Opciones de seleccion 
+ * Clasicas
+ * getElementByld
+ * getElementsByClassName
+ * 
+ * Modernas
+ * Nos permite seleccionar por un selector CSS
+ * selectores CSS
+ * - Etiqueta por ejemplo form
+ * - Clase . por ejemplo .form-control
+ * - Id # por ejemplo #title
+ * 
+ * querySelector() // Si usammos un selecto como clase solo va selecionar la primera coincidencia 
+ * querySelectorAll // Todas
+ */
